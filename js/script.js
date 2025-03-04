b819c2d33ed6358bfdddfb6501400578bb0f3198
media08kada <mediakosongdelapan@gmail.com>
2025-03-05 04:28:49 +0700

    perubahan semboyan

--- js/script.js
+++ js/script.js
@@ -15,24 +15,6 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 };
 
-// Seacrh Aktive
-/*const searchInput = document.getElementById('search-input');
-const list = document.getElementById('list');
-const listItems = list.getElementsByTagName('li');
-
-searchInput.addEventListener('keyup', function (e) {
-  const listTerm = e.target.volue.tolowerCase();
-  for (let i = 0; i < listItems.length; i++) {
-    const text = listItems[i].textContent,tolowerCase()
-    if(text.includes(listItems)){
-      if(text.includes(listTerm)){
-        listItems[i].style.display = 'block';
-      } else {
-        listItems[i].style.display = 'none';
-    }
-  }
-});*/
-
 // Toggle class active untuk shopping cart
 const archive = document.querySelector('.archive');
 document.querySelector('#archive-button').onclick = (e) => {
