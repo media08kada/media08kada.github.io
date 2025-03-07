2eb45e2a096b8c34f3d10b3a9d613d529b873ab5
media08kada <mediakosongdelapan@gmail.com>
2025-03-07 14:45:11 +0700

    update slider layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
+let nextBtn = document.querySelector('.next');
+let prevBtn = document.querySelector('.prev');
+
+let slider = document.querySelector('.slider');
+let sliderList = slider.querySelector('.slider .list');
+let thumbnail = document.querySelector('.slider .thumbnail');
+let thumbnailItems = thumbnail.querySelectorAll('.item');
+
+thumbnail.appendChild(thumbnailItems[0]);
+
+// AutoPlay Slider
+let runAutoPlay = setTimeout(() => {
+  next.click();
+}, 3000);
+
+// Function for next button
+nextBtn.onclick = function () {
+  moveSlider('next');
+};
+
+// Function for prev button
+prevBtn.onclick = function () {
+  moveSlider('prev');
+};
+
+function moveSlider(direction) {
+  let sliderItems = sliderList.querySelectorAll('.item');
+  let thumbnailItems = document.querySelectorAll('.thumbnail .item');
+
+  if (direction === 'next') {
+    sliderList.appendChild(sliderItems[0]);
+    thumbnail.appendChild(thumbnailItems[0]);
+    slider.classList.add('next');
+  } else {
+    sliderList.prepend(sliderItems[sliderItems.length - 1]);
+    thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
+    slider.classList.add('prev');
+  }
+
+  setTimeout(() => {
+    slider.classList.remove('next');
+    slider.classList.remove('prev');
+  }, 2000);
+}
+clearTimeout(runAutoPlay);
+runAutoPlay = setTimeout(() => {
+  next.click();
+}, 3000);
+
+// Remove the event listener after it's triggered once
+
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger menu di klik
