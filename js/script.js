c2903b2d70d33358e2ea31f1fdf1b3c3cac22bed
media08kada <mediakosongdelapan@gmail.com>
2025-03-10 22:03:42 +0700

    tambahan comtinue

--- js/script.js
+++ js/script.js
@@ -142,3 +142,27 @@ if (sliderItems) {
     }
   }, 5000);
 }
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
