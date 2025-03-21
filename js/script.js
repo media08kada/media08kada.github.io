93f868d6870ce3496353de3fa9ea66aa5042676a
media08kada <mediakosongdelapan@gmail.com>
2025-03-21 23:15:49 +0700

    perubahan ukuran

--- js/script.js
+++ js/script.js
@@ -60,6 +60,7 @@ runAutoPlay = setTimeout(() => {
 
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
+
 // ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
