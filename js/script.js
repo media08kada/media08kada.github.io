9b4b23df45be95923467b8537fe5cff5679a5aab
media08kada <mediakosongdelapan@gmail.com>
2025-05-25 21:41:24 +0700

    update

--- js/script.js
+++ js/script.js
@@ -285,3 +285,22 @@ document.querySelector('form[name="media08kada-form"]').addEventListener('submit
   // Mengatur ulang nilai input form
   this.reset();
 });
+
+// Clock
+function updateJam() {
+  const date = new Date();
+  const jam = date.toLocaleTimeString('id-ID', { hour12: false });
+  document.getElementById('jam').innerHTML = jam;
+  setTimeout(updateJam, 1000);
+}
+
+function updateTanggal() {
+  const date = new Date();
+  const tanggal = date.getDate();
+  const bulan = date.toLocaleString('default', { month: 'long' });
+  const tahun = date.getFullYear();
+  document.getElementById('tanggal').innerHTML = `${tanggal} ${bulan} ${tahun}`;
+}
+
+updateJam();
+updateTanggal();
