13450b6544b715206702387d58eef72650f8f7c9
rukunwarga08kada <rukunwarga08kada@gmail.com>
2026-01-15 11:27:21 +0700

    update

--- js/calender.js
+++ js/calender.js
@@ -3,35 +3,36 @@ $(document).ready(function () {
     theme: "Midnight Blue",
     calendarEvents: [
       {
-        id: "event1", // Event's ID (required)
-        name: "New Year", // Event name (required)
-        date: "January/1/2026", // Event date (required)
+        id: "event1", 
+        name: "New Year", 
+        date: "January/1/2026", 
         description:
           "Kegiatan pengawasan lingkungan menjelang pergantian Tahun 2025 dan menyambut Tahun Baru 2026",
-        type: "Panwil", // Event type (required)
-        everyYear: true, // Same event every year (optional)
+        type: "Panwil", 
+        everyYear: true, 
       },
       {
-        id: "event2", // Event's ID (required)
-        name: "Zoom Meeting", // Event name (required)
-        date: "January/09/2026", // Event date (required)
+        id: "event2", 
+        name: "Zoom Meeting", 
+        date: "January/09/2026", 
         description:
           "Zoom Meeting pembahasan Flu vs Superflu, bersama DinKes DKI Jakarta",
-        type: "Webinar", // Event type (required)
-        everyYear: true, // Same event every year (optional)
+        type: "Webinar", 
+        everyYear: true, 
       },
       {
-        id: "event3", // Event's ID (required)
-        name: "Rembuk RW", // Event name (required)
-        date: "January/13/2026", // Event date (required)
+        id: "event3", 
+        name: "Rembuk RW", 
+        date: "January/13/2026", 
         description:
           "Rembuk RW Kelurahan Kelapa Dua Tahun 2026, Tempat Aula Kelurahan Kelapa Dua Lantai 3, Acara pedomanan umum musrenbang",
-        type: "Meeting", // Event type (required)
-        everyYear: true, // Same event every year (optional)
+        type: "Meeting", 
+        everyYear: true, 
       },
-
     ],
   });
 });
+
+
 
 
